fbe6afcb9e99cbda65383f368d76a480c45d3930
Miguel Angel Duran Garcia <miguelangel.duran@scmspain.com>
2016-08-25 18:52:42 +0200

    Improve message for error reading directory and working on binary

--- bin/s3-folder-upload.js
+++ bin/s3-folder-upload.js
@@ -3,3 +3,12 @@
 'use strict'
 
 const s3FolderUpload = require('../')
+const directory = process.argv[2]
+
+if (!directory) {
+  console.log(`[warn] You need to specify a directory located on the root.
+  For example: s3-folder-upload statics`)
+  process.exit(1)
+} else {
+  s3FolderUpload(directory)
+}
